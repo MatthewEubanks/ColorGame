09d657438db87677be98ca10917158f3c78e0614
Matt <spc.eubanks.matthew@gmail.com>
2018-06-04 18:12:13 -0700

    New Commit

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,42 @@
 var colors = generateRandomColors(6);
-  
-  var squares = document.querySelectorAll(".square");
-  var pickedColor = pickColor();
-  var colorDisplay = document.getElementById("colorDisplay");
-  var messageDisplay = document.querySelector('#message');
-  var h1 = document.querySelector("h1");
 
-  colorDisplay.textContent = pickedColor;
-  
-  for(var i = 0; i < squares.length; i++) {
-      //add initial colors to squares
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function () {
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+});
+hardBtn.addEventListener("click", function () {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+});
+
+resetButton.addEventListener("click", function () {
+    //generate all new colors
+    colors = generateRandomColors(6);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colordisplay to match picked color
+    colorDisplay.textContent = pickedColor;
+    //change colors of squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
+    resetButton.textContent = "New Colors";
+});
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
 
     //add click listeners to squares
@@ -19,6 +46,7 @@ var colors = generateRandomColors(6);
         //compare color to pickedColor
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?"
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
         } else {
@@ -26,23 +54,24 @@ var colors = generateRandomColors(6);
             messageDisplay.textContent = "Try Again";
         };
     });
-  };
+};
 
-  function changeColors (color) {
+function changeColors(color) {
     //loop through all squares
-    for(var i = 0; i < squares.length; i++) {
+    for (var i = 0; i < squares.length; i++) {
         //change each color to match given color
         squares[i].style.backgroundColor = color;
-    };    
-  };
-  function pickColor () {
-      //pick a random number
-      var random = Math.floor(Math.random() * colors.length);
-      //use number to access color in display and return it.
-      return colors[random];
-  };
+    };
+};
 
-  function generateRandomColors (num) {
+function pickColor() {
+    //pick a random number
+    var random = Math.floor(Math.random() * colors.length);
+    //use number to access color in display and return it.
+    return colors[random];
+};
+
+function generateRandomColors(num) {
     //make an array 
     var arr = [];
     //add num random colors to array
@@ -53,15 +82,15 @@ var colors = generateRandomColors(6);
     }
     //return array
     return arr;
-  }
+}
 
-  function randomColor () {
-      //pick a red from 0-255
-      var r = Math.floor(Math.random() * 256)
-      //pick a green from 0-255
-      var g = Math.floor(Math.random() * 256)
-      //pick a blue from 0-255
-      var b = Math.floor(Math.random() * 256)
-      //return string
-      return `rgb(${r}, ${g}, ${b})`;
-  }
+function randomColor() {
+    //pick a red from 0-255
+    var r = Math.floor(Math.random() * 256)
+    //pick a green from 0-255
+    var g = Math.floor(Math.random() * 256)
+    //pick a blue from 0-255
+    var b = Math.floor(Math.random() * 256)
+    //return string
+    return `rgb(${r}, ${g}, ${b})`;
+}
